f545c1d2a49b4dbe38ea3eedc554dc965d259204
Lucjan Bryndza <lbryndza.p@boff.pl>
2020-11-07 13:12:04 +0100

    Icestorm updated

--- Formula/icestorm.rb
+++ Formula/icestorm.rb
@@ -1,8 +1,8 @@
 class Icestorm < Formula
   desc "Bitstream Tools for Lattice iCE40 FPGAs"
   homepage "http://www.clifford.at/icestorm/"
-  url "https://github.com/cliffordwolf/icestorm/archive/cd2610e0fa1c6a90e8e4e4cfe06db1b474e752bb.tar.gz"
-  version "20200520"
+  url "https://github.com/cliffordwolf/icestorm/archive/da52117ccd5b4147f64dc7345357ec5439cd7543.tar.gz"
+  version "20201107"
   sha256 "bef024225097944b145b844518d1fee53bab2cc85634252fea09da48fb631cbf"
   head "https://github.com/cliffordwolf/icestorm.git"
   patch :DATA
